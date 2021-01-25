cce3aa7311c7cc3672895dc65e1409d107e9c70d
jeonjunmin <azeros314@naver.com>
2021-01-25 22:34:42 +0900

    part10 - 27:30

--- blog/views.py
+++ blog/views.py
@@ -42,6 +42,11 @@ class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 # def about(request):
 #     return HttpResponse('<h1>Blog About</h1>')
 def about(request):
